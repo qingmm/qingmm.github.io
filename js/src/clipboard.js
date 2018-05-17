5ffa36b5c8341075ad0a3b456c288ccf245e6907
qingmm <739113352@qq.com>
2018-05-17 18:21:39 +0800

    Site updated: 2018-05-17 18:21:39

--- js/src/clipboard.js
+++ js/src/clipboard.js
@@ -1,8 +1,15 @@
 function copy()
 {
-    var Url2=document.getElementById("1").innerText;
+    var answer = '';
+    for(var i = 1; i < 28; i ++) {
+        id = i.toString();
+        value = document.getElementById(id).value;
+        answer += id + '.' + value + ', ';
+    }
+    alert(answer);
     var oInput = document.createElement('input');
-    oInput.value = Url2;
+    oInput.class = 'form-control'
+    oInput.value = answer;
     document.body.appendChild(oInput);
     oInput.select(); // 选择对象
     document.execCommand("Copy"); // 执行浏览器复制命令
