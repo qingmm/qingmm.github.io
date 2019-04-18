0f3ad4b50887a67a69d0c7b58def6bbb39f95546
qingmm <739113352@qq.com>
2019-04-18 20:42:10 +0800

    Site updated: 2019-04-18 20:42:09

--- js/src/clipboard.js
+++ js/src/clipboard.js
@@ -1,7 +1,7 @@
 function copy()
 {
     var answer = '';
-    for(var i = 1; i <= 28; i ++) {
+    for(var i = 1; i <= 26; i ++) {
         id = i.toString();
         value = document.getElementById(id).value;
         answer += id + '.' + value + ', ';
@@ -15,5 +15,19 @@ function copy()
     document.execCommand("Copy"); // 执行浏览器复制命令
     oInput.className = 'oInput';
     oInput.style.display='none';
-    alert('复制成功');
+    alert('提交成功');
+}
+
+function upload()
+{
+    var answer = '';
+    for(var i = 1; i <= 26; i ++) {
+        id = i.toString();
+        value = document.getElementById(id).value;
+        answer += id + '.' + value + ', ';
+    }
+    alert(answer);
+    input = document.getElementById('veditor');
+    input.value = "测试";
+    document.getElementsByClassName('vsubmit')[0].click();
 }
